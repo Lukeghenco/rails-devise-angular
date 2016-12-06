2a660563e03f0e9e908d397e3593a72995b0d4c3
Luke Ghenco <lukeghenco@gmail.com>
2016-12-05 16:54:06 -0800

    adding devise to rails and angular

---
+++ app/assets/javascripts/home/home.controller.js
@@ -0,0 +1,60 @@
+(function() {
+
+    'use strict'
+
+    angular
+        .module('ngDevise')
+        .controller('HomeController', HomeController)
+
+        function HomeController(Auth, $rootScope) {
+            var vm = this
+            vm.logout = Auth.logout
+            vm.login = login
+            vm.register = register
+
+            Auth.currentUser()
+                .then(function(user) {
+                    $rootScope.currentUser = user
+                    debugger;
+                }, function(error) {
+                    console.log(error)
+                })
+
+            function login() {
+                var config = {
+                        headers: {
+                            'X-HTTP-Method-Override': 'POST'
+                        }
+                    }
+
+                Auth.login(vm.userForm, config)
+                    .then(function(user) {
+                        $rootScope.currentUser = user
+                    }, function(error) {
+                        console.log(error)
+                    })
+            }
+
+            function register() {
+                var config = {
+                   headers: {
+                       'X-HTTP-Method-Override': 'POST'
+                   }
+                };
+
+                Auth.register(vm.newUser, config)
+                    .then(function(registeredUser) {
+                        $rootScope.currentUser = registeredUser
+                }, function(error) {
+                   console.log(error)
+                });
+            }
+
+            $rootScope.$on('devise:logout', function(event, user) {
+                $rootScope.currentUser = {}
+            })
+
+
+        }
+
+}())
